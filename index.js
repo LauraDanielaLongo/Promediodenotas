54b4675ee83b6ab590bf22f133693105fa5cd240
Laura Daniela Longo <laauradaniela4@gmail.com>
2022-11-07 11:48:48 -0300

    agregando do while

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-let nota1 = parseInt(prompt('Ingrese la primera nota'));
-let nota2= parseInt (prompt('Ingrese la segunda nota'));
-let nota3= parseInt(prompt('Ingrese la tercera nota'));
-let promedio= (nota1+ nota2+ nota3) / 3 ;
-let promRound= Math.round (promedio)
-if(promRound >= 40){
-    document.write(`Su nota es: ${promRound} Aprobó.`) ;
-}
-else{
-    document.write(`Su nota es: ${promRound} Desaprobó.`);
-}
+const calcularPromedio = (nota1, nota2, nota3) => {
+    let promedio = (nota1 + nota2 + nota3) / 3;
+    return Math.round(promedio);
+    };
+    let nota1=0
+    do{
+        nota1= parseInt(prompt('Ingrese la primera nota'));
+    }
+    while  (isNaN(nota1)); 
+
+    let nota2=0
+    do{
+        nota2= parseInt(prompt('Ingresar la segunda nota'));
+    
+    }
+    while (isNaN(nota2));
+
+    let nota3=0
+    do{
+        nota3=parseInt(prompt("Ingrese la tercera nota"));
+    }
+    while(isNaN(nota3));
+   
+    let promedio = calcularPromedio(nota1, nota2, nota3);
+    if (promedio >= 7) {
+    document.write(`Su nota es: ${promedio} Aprobó.`);
+    }else {
+        document.write(`Su nota es: ${promedio} Desaprobó.`);
+    }
